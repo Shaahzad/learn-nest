17240cac0fd64fa37fb6de45471514329b3b9ffd
Shaahzad <shehzadmemon147@gmail.com>
2026-06-06 07:31:00 +0500

    learn nest js start again

---
+++ src/product/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductService {
+    private products = [
+        {id: 1, name: "mobile", price: 20000},
+        {id: 2, name: "tablet", price: 20000},
+        {id: 3, name: "laptop", price: 20000},
+    ] 
+    getAllProducts(){
+        return this.products
+    }
+    getProductById(id: number){
+        return this.products.find((product) => product.id === id)
+    }
+}
