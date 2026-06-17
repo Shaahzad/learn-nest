5a940c48e2122d35fa1f3c21d1aab3af41125373
Shaahzad <shehzadmemon147@gmail.com>
2026-06-17 22:52:32 +0500

    learn Middleware in nest js

--- src/middleware/logger/logger.middleware.ts
+++ src/middleware/logger/logger.middleware.ts
@@ -1,5 +1,5 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-
+import {Request, Response} from "express"
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
